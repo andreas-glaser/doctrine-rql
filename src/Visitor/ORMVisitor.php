3e56e75d15cbd78c985bd222f03597ab826bdc72
Dominic Tubach <dominic.tubach@to.com>
2016-06-23 10:34:17 +0200

    use setFirstResult() instead of setMaxResults() for offset of LimitNode

--- src/Visitor/ORMVisitor.php
+++ src/Visitor/ORMVisitor.php
@@ -245,7 +245,7 @@ class ORMVisitor
     {
         $this->qb
             ->setMaxResults($node->getLimit())
-            ->setMaxResults($node->getOffset());
+            ->setFirstResult($node->getOffset());
     }
 
     /**
