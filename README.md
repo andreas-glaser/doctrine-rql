a05138fd78a9d819510cb08825961dab85bf2c75
dontub <dtubach@web.de>
2016-07-29 15:00:45 +0200

    merge andreas-glaser/doctrine-rql (#1)
    * Updated xiag/rql-parser dependency in the light of 2.x release
    
    * Created NotNode workaround
    
    * Enabled first tests
    
    * Improved readme

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 #doctrine-rql
 Parses and applies RQL scripts to doctrine query builders.
+
+##Installation
+```shell
+composer require andreas-glaser/doctrine-rql
+```
+
+##Usage
+```php
+<?php
+
+namespace TestApp;
+
+use AndreasGlaser\DoctrineRql\Factory\ORMVisitorFactory;
+
+$rqlString = $_GET['rql']; // ?rql=and(eq(cart.id,123),eq(cart.archived,0))
+
+/** @var QueryBuilder $qb */
+$qb = $entityManager->createQueryBuilder();
+
+$qb
+    ->select('cart')
+    ->from('TestApp\Entity\Cart', 'cart');
+
+// append filters
+ORMVisitorFactory::appendFiltersOnly($queryBuilder, $rqlString, false);
+
+/** @var TestApp\Entity\Cart[] $cartEntities */
+$cartEntities = $qb
+    ->getQuery()
+    ->execute();
+```
--- test/Visitor/ORMTest.php
+++ test/Visitor/ORMTest.php
@@ -51,6 +51,9 @@ class ORMTest extends \PHPUnit_Framework_TestCase
         $executor->execute($loader->getFixtures());
     }
 
+    /**
+     * @author Andreas Glaser
+     */
     public function test()
     {
         $qb = $this->em->createQueryBuilder();
@@ -62,11 +65,21 @@ class ORMTest extends \PHPUnit_Framework_TestCase
             ->leftJoin('products.photos', 'photos')
             ->leftJoin('photos.photo', 'photo');
 
-        $rql = 'eq(cart.id,2)'; // user input
+        $qbTest1 = clone $qb;
 
-        ORMVisitorFactory::appendFiltersOnly($qb, $rql, false);
+        $productName = Helper\StringEncode::encode('Powered Wheelchair X200');
+        $rql = sprintf('and(gt(cart.id,2),eq(cart.products.name,%s))', $productName); // user input
 
-        print_r($qb->getQuery()->getSQL());
-        exit;
+        ORMVisitorFactory::appendFiltersOnly($qbTest1, $rql, false);
+
+        $this->assertEquals('SELECT c0_.id AS id_0, p1_.id AS id_1, p1_.name AS name_2, p2_.id AS id_3, p3_.id AS id_4, p3_.name AS name_5, p2_.product_id AS product_id_6, p2_.photo_id AS photo_id_7 FROM Cart c0_ LEFT JOIN cart_products c4_ ON c0_.id = c4_.cart_id LEFT JOIN Product p1_ ON p1_.id = c4_.product_id LEFT JOIN ProductPhoto p2_ ON p1_.id = p2_.product_id LEFT JOIN Photo p3_ ON p2_.photo_id = p3_.id WHERE c0_.id > ? AND p1_.name = ?', $qbTest1->getQuery()->getSQL());
+
+        $qbTest2 = clone $qb;
+
+        $rql = '(eq(cart.id,string:1)|lt(cart.id,2)|(cart.id<>3&cart.id>=string:4&cart.id=eq=boolean:1))&cart.id!=5&not(or(cart.id=6,eq(cart.id,2)))&cart.id=1&(cart.id==2|cart.id<-3|in(cart.id,(2,float:3)))&sort(+cart.id,-cart.id)&limit(1,2)';
+
+        ORMVisitorFactory::appendFiltersOnly($qbTest2, $rql, false);
+
+        $this->assertEquals('SELECT c0_.id AS id_0, p1_.id AS id_1, p1_.name AS name_2, p2_.id AS id_3, p3_.id AS id_4, p3_.name AS name_5, p2_.product_id AS product_id_6, p2_.photo_id AS photo_id_7 FROM Cart c0_ LEFT JOIN cart_products c4_ ON c0_.id = c4_.cart_id LEFT JOIN Product p1_ ON p1_.id = c4_.product_id LEFT JOIN ProductPhoto p2_ ON p1_.id = p2_.product_id LEFT JOIN Photo p3_ ON p2_.photo_id = p3_.id WHERE (c0_.id = ? OR c0_.id < ? OR (c0_.id <> ? AND c0_.id >= ? AND c0_.id = ?)) AND c0_.id <> ? AND (NOT (c0_.id = ? OR c0_.id = ?)) AND c0_.id = ? AND (c0_.id = ? OR c0_.id < ? OR c0_.id IN (2, 3)) ORDER BY c0_.id DESC LIMIT 1 OFFSET 2', $qbTest2->getQuery()->getSQL());
     }
 }
